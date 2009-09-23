4cd56f55916fe283ee2675ce973347ff2cde74a7
Semyon Chaichenets <sema@interactingstars.org>
2009-09-23 11:55:51 -0600

    started hw2 and hw3

--- make.sh
+++ make.sh
@@ -1,13 +1,7 @@
 export TEXINPUTS="./share/styles//:"
 export TEXFONTS="./share/fonts//:"
 
-#RVC='./random-walk-code.tex' 
-#rm $RVC -f
-#echo '\begin{verbatim}' > $RVC
-#cat ./hw1q3.py >> $RVC
-#echo '\end{verbatim}' >> $RVC
-
-src='hw1'
+src='hw3'
 for i in 1 2; do
 	latex ${src}.tex
 done
