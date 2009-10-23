6f5be10c4b98ba742198d38b3011d846f9e83866
Semyon Chaichenets <sema@interactingstars.org>
2009-10-23 00:13:14 -0600

    assignment 4, as submitted

--- make.sh
+++ make.sh
@@ -1,9 +1,17 @@
 export TEXINPUTS="./share/styles//:"
 export TEXFONTS="./share/fonts//:"
 
-src='hw3'
-for i in 1 2; do
-	latex ${src}.tex
-done
-dvips ${src}.dvi -o
-ps2pdf ${src}.ps
+case $1 in
+	'clean')
+		mv *.ps *dvi *.aux *.log *.pdf /tmp;
+	;;
+	*)
+		src='hw4'
+		for i in 1 2; do
+			latex ${src}.tex
+			pdflatex ${src}.tex
+		done
+		#dvips ${src}.dvi -o
+		#ps2pdf ${src}.ps
+		open ${src}.pdf
+esac
